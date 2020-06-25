1b78116efaaec126a0662b8dd18f8a446cff97f1
cassiano <honoriocassiano@gmail.com>
2020-06-25 16:49:48 -0300

    Add template to infer underlying type of iterable

---
+++ stream.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <type_traits>
+#include <algorithm>
+
+// Stream class
+template <typename Value>
+class Stream {
+
+public:
+    Stream(std::vector<Value> values) : values(values) {}
+
+    ~Stream() {}
+
+private:
+    std::vector<Value> values;
+};
+
+// Utility templates
+template <typename T, typename = std::void_t<decltype(std::begin(std::declval<T>())), decltype(std::end(std::declval<T>()))>>
+auto of(T iterable) {
+
+    typedef typename std::remove_reference<decltype(*std::begin(iterable))>::type Value;
+
+    std::vector<Value> initializer;
+
+    initializer.insert(initializer.end(), std::begin(iterable), std::end(iterable));
+
+    return Stream<Value>(initializer);
+}
+
+// Utility classes
+
+int main(void) {
+
+    std::vector<int> vec{1, 2, 3};
+
+    auto stream = of(vec);
+    
+    return 0;
+}
