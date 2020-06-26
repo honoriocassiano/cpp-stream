5f0ea097552c3240ab555d326dd7a0cf4d87f096
cassiano <honoriocassiano@gmail.com>
2020-06-26 16:45:40 -0300

    Refactor function collect

--- stream.cpp
+++ stream.cpp
@@ -18,7 +18,7 @@ public:
 
     template <typename Mapper>
     auto map(Mapper mapper) {
-        
+
         typedef typename std::result_of<Mapper(Type)>::type NewType;
 
         std::vector<NewType> newValues;
@@ -44,6 +44,16 @@ public:
         return Stream<Type>(newValues);
     }
 
+    template <typename Collector>
+    auto collect(Collector collector) const {
+
+        for (auto it = values.begin(); it != values.end(); it++) {
+            collector.consume(*it);
+        }
+
+        return collector.finish();
+    }
+
 private:
     std::vector<Type> values;
 };
@@ -60,19 +70,57 @@ auto of(T iterable) {
 
     return Stream<Type>(initializer);
 }
-}
 
 // Utility classes
+template <typename T>
+class Collector {
 
+    typedef decltype(*std::begin(std::declval<T>())) ValueType;
+
+public:
+    Collector() {}
+    virtual ~Collector() {}
+
+    void consume(const ValueType& value) {}
+
+    T finish() {}
+};
+
+template <typename ValueType>
+class VectorCollector : public Collector<std::vector<ValueType>> {
+
+public:
+    void consume(const ValueType& value) {
+        values.push_back(value);
+    }
+
+    auto finish() {
+        return values;
+    }
+
+private:
+    std::vector<ValueType> values;
+};
+
+}
+
+// Main function
 int main(void) {
 
     std::vector<int> vec{1, 2, 3};
 
-    auto stream = stream::of(vec);
+    auto stream = stream::of(vec).map([](int value) -> float { return value * 1.2f; });
 
-    auto stream2 = stream.filter([] (int n) -> bool { return n > 2; });
+    auto collector = stream::VectorCollector<float>();
 
+    auto values = stream.collect(collector);
+
+    // auto stream2 = stream.filter([] (int n) -> bool { return n > 2; });
     // auto stream2 = stream.map([] (int value) -> float { return float(value); });
-    
+    for (auto value : values) {
+        std::cout << value << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
